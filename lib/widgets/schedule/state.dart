bedbf42a0eee78854716962a9c046124cf36812c
qhtLucifer <qhtLucifer@foxmail.com>
2023-06-16 20:33:30 +0800

    Update schedule UI.

--- lib/widgets/schedule/state.dart
+++ lib/widgets/schedule/state.dart
@@ -1,8 +1,18 @@
 import '../../entities/class_entity.dart';
 
 class ScheduleState {
-  ClassEntity? lastClass;
-  ClassEntity? onClass;
-  ClassEntity? nextClass;
-  ScheduleState();
+  late List<ClassEntity> allClasses;
+  ScheduleState() {
+    allClasses = [];
+
+    ///TODO :TEST
+    allClasses = List.filled(10,   ClassEntity(
+        name: '测试课程1fsdfsdf',
+        beginTime: '12:30',
+        endTime: '12:30',
+        location: '火星',
+        info: '《软件工程》是信息与计算科学专业的专业限选课程。通过本课程的学习，学生能够使用软件工程的方法进行软件项目的分析、设计、实现和测试，旨在培养学生具备软件工程思想及实际软件开发的能力。本课程主要内容包括软件工程与软件过程、软件需求分析与建模、软件设计与建模、软件编程与软件测试。',
+        id: '00001',
+        teacherName: '秦浩天'),);
+  }
 }
--- lib/widgets/schedule/view.dart
+++ lib/widgets/schedule/view.dart
@@ -1,98 +1,180 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:iupc/constants.dart';
+import 'package:iupc/entities/class_entity.dart';
 import 'package:iupc/widgets/schedule/state.dart';
 import 'logic.dart';
 
-class SchedulePage extends StatelessWidget {
-  final double height;
+class SchedulePage extends StatefulWidget {
+  final double width;
 
-  SchedulePage({Key? key, required this.height}) : super(key: key);
-
-  final logic = Get.put(ScheduleLogic());
-  final state = Get
-      .find<ScheduleLogic>()
-      .state;
+  const SchedulePage({Key? key, required this.width}) : super(key: key);
 
   @override
+  State<SchedulePage> createState() => _SchedulePageState();
+}
+
+class _SchedulePageState extends State<SchedulePage> {
+  final logic = Get.put(ScheduleLogic());
+  final state = Get.find<ScheduleLogic>().state;
+  bool isExpanded = false;
+  GlobalKey key = GlobalKey();
+  late double minHeight;
+  late double expandedHeight;
+  @override
   Widget build(BuildContext context) {
-    return Stack(
+    minHeight =
+        state.allClasses.length >= 3 ? 196 : 58.0 * state.allClasses.length;
+    expandedHeight = 58.0 * state.allClasses.length;
+    return Column(
+      children: [
+        Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              '今日课程',
+              style: Theme.of(context).textTheme.titleMedium,
+            )),
+        Container(
+          width: widget.width,
+          // height: (isExpanded?264+34.0*state.allClasses.length:baseHeight),
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              borderRadius: const BorderRadius.all(Radius.circular(14))),
+
+          child: Column(
             children: [
-              Container(
-                height: height,
-                width: height * 0.7,
-                decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.secondaryContainer,
-                    borderRadius: BorderRadius.only(
-                      // topLeft: Radius.zero,
-                      //   bottomLeft: Radius.zero,
-                        topRight: Radius.circular(height * 0.3),
-                        bottomRight: Radius.circular(height * 0.3))),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
                 child: Flex(
-                  direction: Axis.vertical,
+                  direction: Axis.horizontal,
                   children: [
                     Flexible(
-                      flex: 1,
-                      child: ClassBeside(
-                        classLocation: '南教302',
-                        className: '神经网络与深度学习',
-                        beginTime: '8:00',
-                        endTime: '10:35',
-                        status: 1,
-                        state: state,
-                        logic: logic,
+                      flex: 2,
+                      child: Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text(
+                              DateTime.now().day.toString(),
+                              style: Theme.of(context).textTheme.displayLarge,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+                              child: Text(
+                                '日',
+                                style: Theme.of(context).textTheme.labelSmall,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                     Flexible(
-                      flex: 1,
-                      child: Container(),
-                    ),
-                    Flexible(
-                        flex: 1, child: ClassBeside(
-                      classLocation: '东廊201',
-                      className: '软件工程',
-                      beginTime: '10:55',
-                      endTime: '12:20',
-                      status: -1,
-                      state: state,
-                      logic: logic,
-                    )),
+                      flex: state.allClasses.isNotEmpty ? 3 : 2,
+                      child: state.allClasses.isNotEmpty
+                          ? Column(
+                              children: [
+                                AnimatedContainer(
+                                    duration: const Duration(milliseconds: 400),
+                                    curve: Curves.easeInCirc,
+                                    width: widget.width,
+                                    height: (isExpanded
+                                        ? expandedHeight
+                                        : minHeight),
+                                    child: _generateSchedule()),
+                              ],
+                            )
+                          : Center(
+                              child: Row(
+                              children: [
+                                const Text('今日暂无课程'),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Icon(
+                                    Icons.tag_faces_outlined,
+                                    color:
+                                        Theme.of(context).colorScheme.tertiary,
+                                  ),
+                                )
+                              ],
+                            )),
+                    )
                   ],
                 ),
               ),
-              SizedBox(
-                height: height,
-                width: height * 0.9,
-                child: Flex(
-                  direction: Axis.vertical,
-                  children: [
-                    const Spacer(),
-                    Flexible(
-                      child: Container(
-                        height: height,
-                        width: height ,
-                        decoration: BoxDecoration(
-                            color: Theme.of(context).colorScheme.tertiaryContainer,
-                            borderRadius: BorderRadius.only(
-                              // topLeft: Radius.zero,
-                              //   bottomLeft: Radius.zero,
-                                topRight: Radius.circular(height),
-                                bottomRight: Radius.circular(height))),
-                        child: ClassBeside(
-                          className: '大数据管理',
-                          classLocation: '南教201',
-                          beginTime: '14:00',
-                          endTime: '15:35',
-                          status: 0,
-                          state: state,
-                          logic: logic,),
-                      ),
-                    ),
-                    const Spacer(),
-                  ],
-                ),
-              )
+              state.allClasses.length > 3
+                  ? Row(
+                      children: [
+                        const Expanded(child: Text('')),
+                        AnimatedSwitcher(
+                            transitionBuilder: (child, anim) {
+                              return ScaleTransition(
+                                scale: anim,
+                                child: child,
+                              );
+                            },
+                            duration: const Duration(milliseconds: 400),
+                            child: IconButton(
+                                key: ValueKey(isExpanded),
+                                onPressed: () {
+                                  setState(() {
+                                    isExpanded = !isExpanded;
+                                  });
+                                },
+                                icon: Icon(!isExpanded
+                                    ? Icons.fullscreen_rounded
+                                    : Icons.fullscreen_exit_rounded))),
+                      ],
+                    )
+                  : Container(),
             ],
-          );
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _generateSchedule() {
+    return ListView(
+      physics: isExpanded? const NeverScrollableScrollPhysics():null,
+      shrinkWrap: isExpanded,
+      children: state.allClasses.map((e) => _scheduleTile(e)).toList(),
+    );
+  }
+
+  Widget _scheduleTile(ClassEntity classEntity) {
+    return Container(
+      padding: const EdgeInsets.only(top: 8, bottom: 8.0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Icon(Icons.class_outlined,size: 16,color: Theme.of(context).colorScheme.tertiary,),
+              SizedBox(
+                  width: checkIslandScape(context)? 84:64,
+                  child: Text(
+                    classEntity.name,
+                    overflow: TextOverflow.ellipsis,
+                  )),
+              const Expanded(child: Text('')),
+               Icon(Icons.person_outline,size: 16,color: Theme.of(context).colorScheme.tertiary,),
+              Text(classEntity.teacherName ?? '暂无教师信息')
+            ],
+          ),
+          Row(
+            children: [
+               Icon(Icons.schedule,size: 16,color: Theme.of(context).colorScheme.tertiary,),
+              Text('${classEntity.beginTime}-${classEntity.endTime}'),
+              const Expanded(child: Text('')),
+               Icon(Icons.location_city_outlined,size: 16,color: Theme.of(context).colorScheme.tertiary,),
+              Text(classEntity.location),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
 
@@ -107,7 +189,7 @@ class ClassBeside extends StatelessWidget {
     Container(
       padding: const EdgeInsets.all(5),
       decoration:
-      const BoxDecoration(shape: BoxShape.circle, color: Colors.orange),
+          const BoxDecoration(shape: BoxShape.circle, color: Colors.orange),
       child: const Center(
         child: Icon(
           Icons.circle_outlined,
@@ -118,7 +200,7 @@ class ClassBeside extends StatelessWidget {
     Container(
       padding: const EdgeInsets.all(5),
       decoration:
-      const BoxDecoration(shape: BoxShape.circle, color: Colors.green),
+          const BoxDecoration(shape: BoxShape.circle, color: Colors.green),
       child: const Center(
         child: Icon(
           Icons.school_outlined,
@@ -129,7 +211,7 @@ class ClassBeside extends StatelessWidget {
     Container(
       padding: const EdgeInsets.all(5),
       decoration:
-      const BoxDecoration(shape: BoxShape.circle, color: Colors.orange),
+          const BoxDecoration(shape: BoxShape.circle, color: Colors.orange),
       child: const Center(
         child: Icon(
           Icons.done,
@@ -148,70 +230,74 @@ class ClassBeside extends StatelessWidget {
     required this.classLocation,
     required this.beginTime,
     required this.endTime,
-    required this.status, required this.logic, required this.state,
+    required this.status,
+    required this.logic,
+    required this.state,
   });
 
   @override
   Widget build(BuildContext context) {
-      return Container(
-        padding: status == 0 ? const EdgeInsets.only(left: 8) : const EdgeInsets
-            .only(left: 8, top: 4, bottom: 4),
-        child: Flex(
-          direction: Axis.vertical,
-          children: [
-            Flexible(
+    return Container(
+      padding: status == 0
+          ? const EdgeInsets.only(left: 8)
+          : const EdgeInsets.only(left: 8, top: 4, bottom: 4),
+      child: Flex(
+        direction: Axis.vertical,
+        children: [
+          Flexible(
+            child: Row(
+              children: [
+                Icon(Icons.access_time_outlined,
+                    color: Theme.of(context).colorScheme.onSecondaryContainer),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Text(
+                    "$beginTime-$endTime",
+                  ),
+                )
+              ],
+            ),
+          ),
+          Flexible(
+            flex: 2,
+            child: Padding(
+              padding: status == 0
+                  ? const EdgeInsets.only(right: 16)
+                  : const EdgeInsets.only(left: 8, right: 32),
               child: Row(
                 children: [
-                  Icon(Icons.access_time_outlined,
-                      color: Theme.of(context).colorScheme.onSecondaryContainer),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: Text(
-                      "$beginTime-$endTime",
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Flexible(
-              flex: 2,
-              child: Padding(
-                padding: status == 0
-                    ? const EdgeInsets.only(right: 16)
-                    : const EdgeInsets.only(left: 8, right: 32),
-                child: Row(
-                  children: [
-
-                    Expanded(
-                      child: Center(
-                        child: Text(
-                          className,
-                          style: (const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              overflow: TextOverflow.ellipsis)),
-                        ),
+                  Expanded(
+                    child: Center(
+                      child: Text(
+                        className,
+                        style: (const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            overflow: TextOverflow.ellipsis)),
                       ),
                     ),
-                    statusIconList[status + 1]
-                  ],
-                ),
-              ),
-            ),
-            Flexible(
-              child: Row(
-                children: [
-                  Icon(Icons.location_city,
-                    color:Theme.of(context).colorScheme.onSecondaryContainer,),
-                  Padding(
-                      padding: const EdgeInsets.only(left: 8),
-                      child: Text(
-                        classLocation,
-                      )),
+                  ),
+                  statusIconList[status + 1]
                 ],
               ),
-            )
-          ],
-        ),
-      );
+            ),
+          ),
+          Flexible(
+            child: Row(
+              children: [
+                Icon(
+                  Icons.location_city,
+                  color: Theme.of(context).colorScheme.onSecondaryContainer,
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: Text(
+                      classLocation,
+                    )),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
